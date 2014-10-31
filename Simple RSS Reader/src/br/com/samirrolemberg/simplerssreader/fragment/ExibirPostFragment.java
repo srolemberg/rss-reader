e554cb0922bbfb3352f37cc1ac578f4de45f97dc
Samir Rolemberg <srolemberg@outlook.com>
2014-10-31 18:51:05 -0200

    Implementação do Botão de Video e adição da api do Jsoup

--- Simple RSS Reader/src/br/com/samirrolemberg/simplerssreader/fragment/ExibirPostFragment.java
+++ Simple RSS Reader/src/br/com/samirrolemberg/simplerssreader/fragment/ExibirPostFragment.java
@@ -2,20 +2,38 @@ package br.com.samirrolemberg.simplerssreader.fragment;
 
 import java.util.List;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Attribute;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Fragment;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.widget.Toast;
 import br.com.samirrolemberg.simplerssreader.R;
 import br.com.samirrolemberg.simplerssreader.conn.DatabaseManager;
 import br.com.samirrolemberg.simplerssreader.dao.DAOConteudo;
 import br.com.samirrolemberg.simplerssreader.dao.DAODescricao;
+import br.com.samirrolemberg.simplerssreader.dialog.DetalhesPerguntaDialogPost;
 import br.com.samirrolemberg.simplerssreader.model.Conteudo;
 import br.com.samirrolemberg.simplerssreader.model.Descricao;
 import br.com.samirrolemberg.simplerssreader.model.Post;
+import br.com.samirrolemberg.simplerssreader.u.U;
 
 public class ExibirPostFragment extends Fragment {
 	/**
@@ -63,6 +81,55 @@ public class ExibirPostFragment extends Fragment {
 		DatabaseManager.getInstance().closeDatabase();
 
 	}
+	private class JSInterface {
+		private Activity activity;
+		@SuppressWarnings("unused")
+		private JSInterface() {
+			new Exception();
+		}
+		public JSInterface(Activity activity){
+			this.activity = activity;
+		}
+		@JavascriptInterface
+		public void toast(String toast){
+			Toast.makeText(activity, toast, Toast.LENGTH_LONG).show();
+		}
+		
+		@SuppressLint("InflateParams")
+		@JavascriptInterface
+		public void abrir(String url){
+			LayoutInflater inflater = activity.getLayoutInflater();
+			View pergunta = (new DetalhesPerguntaDialogPost(activity,  inflater.inflate(R.layout.dialog_pergunta_feed, null), postAux,"Como deseja abrir o vídeo?")).create();
+			final Uri uri = Uri.parse(url);
+			final Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+			new AlertDialog.Builder(activity)
+			.setView(pergunta)
+			.setPositiveButton("Navegador", new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					open(intent);
+				}
+			})
+			.setNegativeButton("Aplicativo", new OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					intent.setDataAndType(uri, "video/mp4");
+					open(intent);
+				}
+			})
+			.show();
+		}
+		
+		private void open(final Intent intent){
+			if (U.isConnected(activity)) {
+				startActivity(intent);				
+			}else{
+				Toast.makeText(activity, "Não há conexão de internet.", Toast.LENGTH_SHORT).show();					
+			}
+		}
+		
+	}
+	@SuppressLint("SetJavaScriptEnabled")
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.fragment_exibir_post, container, false);
@@ -71,28 +138,29 @@ public class ExibirPostFragment extends Fragment {
 		daoConteudo = new DAOConteudo(getActivity());
 		daoDescricao = new DAODescricao(getActivity());
 		
+		navegador.getSettings().setBlockNetworkLoads(true);
+		navegador.getSettings().setJavaScriptEnabled(true);
+		navegador.setWebChromeClient(new WebChromeClient());
+		navegador.addJavascriptInterface(new JSInterface(getActivity()), "Android");
+		
 		if (daoConteudo.size(postAux)>0) {//se o conteudo total não é nulo
 			String data = conteudosFrag.get(0).getValor();
-			//String mimeType = conteudosFrag.get(0).getTipo();
 			String mimeType = "text/html";
-			navegador.getSettings().setBlockNetworkLoads(true);
-			//navegador.getSettings().setJavaScriptEnabled(true);
-			//navegador.loadData(data, "text/"+mimeType+"; charset=UTF-8",null);
-			//navegador.loadData(data, "text/"+mimeType+"; charset=UTF-8",null);
+			data = data.replace("src=\"//", "src=\"http://");
+
+			data = returnData(data);
+			
 			navegador.loadData(data, mimeType+"; charset=UTF-8",null);
-			//navegador.setText(Integer.toString(getArguments().getInt(ARG_POST_CONTENT)));
-			Log.w("MAVEGADOR", "daoConteudo");
 			Log.d("MAVEGADOR", data);
 		}else if (daoDescricao.size(postAux)>0){//se a não há conteudo mas há descrição
 			String data = descricoesFrag.get(0).getValor();
-			//String mimeType = descricoesFrag.get(0).getTipo();
+			data = data.replace("src=\"//", "src=\"http://");
+
 			String mimeType = "text/html";
-			navegador.getSettings().setBlockNetworkLoads(true);
-			//navegador.getSettings().setJavaScriptEnabled(true);
-			//navegador.loadData(data, "text/"+mimeType+"; charset=UTF-8",null);
+			
+			data = returnData(data);
+
 			navegador.loadData(data, mimeType+"; charset=UTF-8",null);
-			//navegador.setText(Integer.toString(getArguments().getInt(ARG_POST_CONTENT)));
-			Log.w("MAVEGADOR", "daoDescricao");
 			Log.d("MAVEGADOR", data);
 		}
 		DatabaseManager.getInstance().closeDatabase();
@@ -100,6 +168,34 @@ public class ExibirPostFragment extends Fragment {
 		
 		setHasOptionsMenu(true);
 		return rootView;
+	}
+	
+	private String returnData(String data){
+		Document doc = Jsoup.parse(data);
+		
+		Element elem = doc.head();
+		String script = "<script type=\"text/javascript\">"+
+		"function toast(toast) {"+
+		"    Android.abrir(toast);"+
+		"}"+
+		"</script>";
+		elem.append(script);
+		
+		Elements elements = doc.select("iframe");
+		elements.tagName("button");
+		int i = 1;
+		for (Element element : elements) {
+			String src = element.getElementsByTag("button").attr("src");
+
+			for (Attribute attr : element.attributes().asList()) {
+				element.removeAttr(attr.getKey());
+			}
+			element.attr("onClick", "toast('"+src+"')");
+			element.text("Assistir ao Video "+i);
+			i++;
+		}
+		
+		return data = doc.html();
 	}
 
 }
